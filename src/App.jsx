e51db7fb0e241d17e2f44cc926882664192791a7
Jasmin Inneh <110208655+JasminInneh@users.noreply.github.com>
2024-02-04 18:12:55 +0000

    added a new port for the database to run on. Now experiencing issue with accessing data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {useState, useEffect} from "react"
 import Nav from './components/Nav'
 import Header from './components/Header'
 import Feed from './components/Feed'
@@ -5,13 +6,38 @@ import PopUp from './components/PopUp'
 
 
 const App = () => {
+  const [user, setUser] = useState(null)
+
+  const userId = "121dc7b7-abec-452f-9fb4-5ff9e052c482"
+
+  const getUser = async () => {
+    
+    try {
+      const response = await fetch(``)
+      const data = await response.json()
+      setUser(data[0])
+
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+  console.log(user)
+
+
   return (
-      <div className="app">
-        <Nav/>
+    <>
+      {user && <div className="app">
+        <Nav url={user.instagram_url}/>
         <Header />
         <Feed/>
         {/* <PopUp/> */}
-      </div>
+      </div>}
+    </>
+   
   )
 }
 
